551c86cb58fb076b1c56202b04687e3d2b028f5a
Victor Hugo Tirado Peñaranda <victorpr7330@gmail.com>
2026-05-11 09:23:00 -0400

    fix: base condicional solo para produccion

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/honorarios-tbs/',
+  base: process.env.NODE_ENV === 'production' ? '/honorarios-tbs/' : '/',
   plugins: [react()],
 })
